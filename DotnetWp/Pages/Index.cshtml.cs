3349f94e753af77a1982de2e1c8435364dd08dc8
Eduardo Beckford <Eduardo.Beckford@controlrisks.com>
2021-08-31 10:34:53 -0500

    test fetching from db

--- DotnetWp/Pages/Index.cshtml.cs
+++ DotnetWp/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotnetWp.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,15 +12,18 @@ namespace DotnetWp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly WordPressDbContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, WordPressDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public void OnGet()
         {
-
+            var post = _db.Posts.FirstOrDefault();
+            ViewData["post"] = post.ToString();
         }
     }
 }
